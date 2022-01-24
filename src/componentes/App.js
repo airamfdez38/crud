033d44b94792d3d30be4bddf99c10aedd2b7f733
maria <airamfdezs@gmailcom>
2022-01-24 12:56:09 +0100

    funcionalidad 1

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,24 +1,40 @@
+import React, { useState } from 'react';
 import './App.css';
-import Create from './Create/create.js';
-import Read from './Read/read.js';
+import Create from './Create/Create.js'
+import Read from './Read/Read.js'
 
-const elems = [
-    {
-        text: 'Hacer CRUD',
-        done: true
+const initialElems = [
+    { 
+        text: 'Hacer TicTacToe', 
+        done: true 
     },
-    {
-        text: 'Personalizar CRUD',
-        done: false
+    { 
+        text: 'Hacer CRUD', 
+        done: false 
+    },
+    { 
+        text: 'Personalizar TicTacToe', 
+        done: true 
+    },
+    { 
+        text: 'Personalizar CRUD', 
+        done: false 
     }
-]
+];
 
-const App = () => (
-  <div className="App">
-    <Create/>
-    <Read elems={elems}/>
-    <button className='crud-clear' type='button'>Borrar hechos</button>
-  </div>
-);
+const App = () => {
+
+  const [elems, setElems] = useState( initialElems)
+
+  return(
+    <div className="app">
+    <h1>CRUD</h1>
+    <Create  onNewElem={elem => setElems([...elems, elem])}/>
+    <Read elems={elems} onElemsChange={elems => setElems(elems)}/>
+    <button className="crud-clear" type="button" onClick={() => setElems(elems.filter(elem => !elem.done))}>Borrar</button>
+</div>
+  )
+
+};
 
 export default App;
---
+++ src/componentes/Create/Create.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Create = ({onNewElem}) => {
+    
+    const [newElemText, setNewElemText] = useState('');
+
+    return (
+        <fieldset className="crud-input">
+            <legend className="crud-input__label">Nuevo elemento</legend>
+            <input className="crud-input__input"
+            value={newElemText}
+            onChange={e=> setNewElemText(e.target.value)}
+            onKeyUp={
+                (e)=>{
+                    if(e.key==='Enter' && newElemText.length){
+                        onNewElem({done:false, text: newElemText});
+                        setNewElemText('');
+                    }
+                }
+            }
+            placeholder="Introduzca el elemento" />
+
+        </fieldset>)
+    
+};
+
+export default Create;
